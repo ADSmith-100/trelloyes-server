477fdb23203f644c0a7342a7e873d20a7454eb89
Adam Smith <coloradoasmith@gmail.com>
2020-09-06 15:37:48 -0600

    refactor is working

---
+++ src/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  PORT: process.env.PORT || 8000,
+  NODE_ENV: process.env.NODE_ENV || "development",
+};
+
+//We can add API_TOKENS and other secrets to this file in the future if and when needed.
